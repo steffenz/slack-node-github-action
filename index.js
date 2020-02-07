b7402728ab77e95d1d539a3f29193c247731e981
Steffen Martinsen <steffen@steffenmedia.no>
2020-02-07 13:25:14 +0100

    Tried to rewrite environment check

--- index.js
+++ index.js
@@ -2,10 +2,10 @@ const request = require('request');
 
 const REQUIRED_ENV_VARS = [
   'SLACK_TOKEN',
-  'SLACK_MESSAGE'
+  'SLACK_MESSAGE',
 ];
 
-var env = process.env.NODE_ENV || 'dev';
+var env = process.env.ENVIRONMENT || 'dev';
 if(env === 'dev'){  require('dotenv').config(); }
 
 REQUIRED_ENV_VARS.forEach(name => {
@@ -13,6 +13,7 @@ REQUIRED_ENV_VARS.forEach(name => {
     console.error(`Missing required enviroment variable ${name}`)
   }
 });
+
 
 const slackUrl = `https://hooks.slack.com/services/${process.env.SLACK_TOKEN}`;
 const text = process.env.SLACK_MESSAGE;
